06a038098075a527e90ef7bda84ec96a83bac2e0
utopiabeam <utopiabeam@gmail.com>
2018-09-26 14:55:47 +0700

    Update range_insert solution

--- Solutions/d59_q1_range_insert/student.h
+++ Solutions/d59_q1_range_insert/student.h
@@ -4,20 +4,13 @@
 
 template <typename T>
 void CP::vector<T>::insert(iterator position, iterator first, iterator last) {
-  CP::vector<T> tmp;
-  for (iterator it = begin(); it != position; it++) {
-    tmp.push_back(*it);
-  }
+  std::vector<T> tmp(position, end());
+  resize(position-begin());
   for (iterator it = first; it != last; it++) {
-    tmp.push_back(*it);
-  }
-  for (iterator it = position; it != end(); it++) {
-    tmp.push_back(*it);
-  }
-
-  clear();
-  for (iterator it = tmp.begin(); it != tmp.end(); it++) {
     push_back(*it);
+  }
+  for (auto &x: tmp) {
+    push_back(x);
   }
 }
 
