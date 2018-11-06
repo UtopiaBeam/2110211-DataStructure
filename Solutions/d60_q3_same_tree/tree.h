5a748909fc4cc0d74c63d0d1acb19796fa3dd694
utopiabeam <utopiabeam@gmail.com>
2018-11-06 09:34:42 +0700

    Add solution for d60_q3_same_tree

---
+++ Solutions/d60_q3_same_tree/tree.h
@@ -0,0 +1,65 @@
+#ifndef TREE_H_INCLUDED
+#define TREE_H_INCLUDED
+
+class Tree{
+    class Node{
+        public:
+            friend class Tree;
+            Node() {data = -1; left = NULL; right = NULL;}
+            Node(const int x, Node* left, Node* right) : data(x), left(left), right(right) {}
+
+        protected:
+            int data;
+            Node* left;
+            Node* right;
+    };
+public:
+
+    Tree() {
+        mRoot = NULL;
+        mSize = 0;
+    }
+
+    ~Tree() {
+        clear(mRoot);
+    }
+
+    void clear(Node*& r) {
+        if (!r) return;
+        clear(r->left);
+        clear(r->right);
+        delete r;
+    }
+
+    void insert(int x) {
+        insertAt(mRoot, x);
+    }
+    bool isSameBinaryTree(Tree& t) {
+        return isSameBinaryTree(this->mRoot, t.mRoot);
+    }
+    
+    bool isSameBinaryTree(Node*& t1, Node*& t2) {
+        if (t1 == NULL && t2 == NULL)
+            return true;
+        if (t1->data != t2->data)   return false;
+        return isSameBinaryTree(t1->left, t2->left) &&
+                isSameBinaryTree(t1->right, t2->right);
+    }
+
+protected:
+    void insertAt(Node*& r, int x) {
+        if (!r) {
+           r = new Node(x, NULL, NULL);
+           mSize++;
+           return;
+        }
+        if (r->data == x) return; // Do nothing
+        if (x < r->data) insertAt(r->left, x); else
+        insertAt(r->right, x);
+    }
+    Node* mRoot;
+    int mSize;
+
+};
+
+#endif // TREE_H_INCLUDED
