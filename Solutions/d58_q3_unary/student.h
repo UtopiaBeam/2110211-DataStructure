fc752822ec10c52425cec265fa40711165dffc48
utopiabeam <utopiabeam@gmail.com>
2018-11-08 09:19:06 +0700

    Add solution for d58_q3_unary

---
+++ Solutions/d58_q3_unary/student.h
@@ -0,0 +1,22 @@
+#ifndef __STUDENT_H__
+#define __STUDENT_H__
+
+template <typename KeyT,
+          typename MappedT,
+          typename CompareT>
+size_t CP::map_bst<KeyT,MappedT,CompareT>::process(node* ptr) const {
+  if (ptr == NULL)    return 0;
+  size_t cnt = process(ptr->left) + process(ptr->right);
+  if (ptr->left == NULL ^ ptr->right == NULL)
+    return 1 + cnt;
+  return cnt;
+}
+
+template <typename KeyT,
+          typename MappedT,
+          typename CompareT>
+size_t CP::map_bst<KeyT,MappedT,CompareT>::count_unary() const {
+  return process(mRoot);
+}
+
+#endif
